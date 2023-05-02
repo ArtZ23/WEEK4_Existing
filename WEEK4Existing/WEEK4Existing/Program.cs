904256931c433bd97ffa4dae5d218f0842915914
ArtZ23 <zzartur77@gmail.com>
2023-05-02 22:53:29 +0400

    Test change

--- WEEK4Existing/WEEK4Existing/Program.cs
+++ WEEK4Existing/WEEK4Existing/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Please Enter UserName");
+var userName = Console.ReadLine();
+Console.WriteLine("You entered username" + userName);
